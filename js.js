fd2cc0d19ae19196126113c0f034bb037cdaabc9
Ososese-A <contact.sese.a@gmail.com>
2024-02-08 13:43:51 +0200

    All we have done with js so far

---
+++ js.js
@@ -0,0 +1,9 @@
+const par = (name) => {
+    myName = name;
+    console.log("My name is" + " " + myName)
+}
+
+par("Jane")
+
+
+
---
+++ python.py
@@ -0,0 +1,5 @@
+def par (name):
+    my_name = name
+    print("My name is", my_name)
+
+par("jane")
